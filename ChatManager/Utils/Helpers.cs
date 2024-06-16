e15f2fc1bd365654ae0b320a8574b68b5d295014
nekitalek <nekitness@gmail.com>
2024-06-16 16:43:28 +0300

    regex add

--- ChatManager/Utils/Helpers.cs
+++ ChatManager/Utils/Helpers.cs
@@ -56,16 +56,53 @@ public class Helpers
         return target;
 
     }
-    
+
     public static string FilterAds(string target)
     {
-        
+        // Регулярные выражения для различных типов ссылок и IP-адресов
         string ipRegexPattern = @"\b(?:\d{1,3}\.){3}\d{1,3}\b";
-        string urlRegexPattern = @"(?:http(s)?://)?[\w.-]+\.[a-zA-Z]{2,}(?:/\S*)?";
+        string generalUrlPattern = @"(?:https?:\/\/)?(?:www\.)?[\w\-]+\.[\w\-]+(?:\/\S*)?";
+        string neverlosePattern = @"(?:https?:\/\/)?(?:www\.)?neverlose\.cc\/market\/item\?id=\w+";
+        string marketPattern = @"(?:https?:\/\/)?(?:www\.)?market\/item\?id=\w+";
+        string itemPattern = @"(?:https?:\/\/)?(?:www\.)?item\?id=\w+";
+        string idPattern = @"(?:https?:\/\/)?(?:www\.)?\?id=\w+";
 
-        target = Regex.Replace(target, ipRegexPattern, "****", RegexOptions.IgnoreCase);
-        return Regex.Replace(target, urlRegexPattern, "****", RegexOptions.IgnoreCase);
-        
+        // Регулярные выражения для социальных сетей (Facebook, Twitter, Instagram и т.д.)
+        string socialMediaPattern = @"(?:https?:\/\/)?(?:www\.)?(?:facebook|twitter|instagram|tiktok|vk|ok|youtube|telegram|whatsapp|viber)\.com(?:\/\S*)?";
+
+        // Регулярные выражения для попыток обхода фильтров (например, добавление пробелов, точек и подчеркиваний)
+        string obfuscationPattern = @"(?:https?\s*[:.]\s*\/{2})?(?:www\.)?[\w\-]+\s*[\.:]\s*[\w\-]+(\s*[\.:]\s*[a-z]{2,})(\s*\/\S*)?";
+
+        // Дополнительные выражения для вариаций ссылок
+        string partialNeverlosePattern = @"(?:neverlose\.cc\/market\/item\?id=\w+)";
+        string partialMarketPattern = @"(?:market\/item\?id=\w+)";
+        string partialItemPattern = @"(?:item\?id=\w+)";
+        string partialIdPattern = @"(?:\?id=\w+)";
+
+        // Объединение всех регулярных выражений в один список
+        List<string> patterns = new List<string>
+    {
+        ipRegexPattern,
+        generalUrlPattern,
+        neverlosePattern,
+        marketPattern,
+        itemPattern,
+        idPattern,
+        socialMediaPattern,
+        obfuscationPattern,
+        partialNeverlosePattern,
+        partialMarketPattern,
+        partialItemPattern,
+        partialIdPattern
+    };
+
+        // Применение всех регулярных выражений к строке target
+        foreach (var pattern in patterns)
+        {
+            target = Regex.Replace(target, pattern, "****", RegexOptions.IgnoreCase);
+        }
+
+        return target;
     }
-    
+
 }
